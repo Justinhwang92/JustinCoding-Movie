25a10732541500254626a0852916050b6a882b7b
Justin <justinhwang92@gmail.com>
2021-07-29 12:06:09 -0400

    Fetch movie data from API

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+// API
+import API from "../API";
 // Config
 import { POTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 // Components
@@ -12,6 +14,32 @@ const Home = () => {
   const [state, setState] = useState();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+
+  const fetchMovies = async (page, searchTerm = "") => {
+    try {
+      setError(false);
+      setLoading(true);
+
+      const movies = await API.fetchMovies(searchTerm, page);
+      // console.log(movies);
+
+      setState((prev) => ({
+        ...movies,
+        results:
+          page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
+      }));
+    } catch (error) {
+      setError(true);
+    }
+    setLoading(false);
+  };
+
+  // console.log(state);
+
+  // Initial render
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
 
   return <div>Home Page</div>;
 };
