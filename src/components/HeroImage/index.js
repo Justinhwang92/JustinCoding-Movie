b0dda715085ab28ca23455a21f35438ba1a427f4
Justin <justinhwang92@gmail.com>
2021-07-31 13:36:36 -0400

    Fix image is not showing up

--- src/components/HeroImage/index.js
+++ src/components/HeroImage/index.js
@@ -3,8 +3,9 @@ import React from "react";
 import { Wrapper, Content, Text } from "./HeroImage.styles";
 
 const HeroImage = ({ image, title, text }) => {
+  console.log(image);
   return (
-    <Wrapper iamge={image}>
+    <Wrapper image={image}>
       <Content>
         <Text>
           <h1>{title}</h1>
