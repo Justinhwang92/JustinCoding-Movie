988bd3873527d7688ac5a6e3ec99416903b8c136
Justin <justinhwang92@gmail.com>
2021-08-04 21:47:27 -0400

    Change Logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 // Logos
-import RMDBLogo from "../../images/react-movie-logo.svg";
+import JCLogo from "../../images/jc-logo.svg";
 import TMDMBogo from "../../images/tmdb_logo.svg";
 
 import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
@@ -10,7 +10,7 @@ const Header = () => (
   <Wrapper>
     <Content>
       <Link to="/">
-        <LogoImg src={RMDBLogo} alt="rmdb-logo" />
+        <LogoImg src={JCLogo} alt="rmdb-logo" />
       </Link>
       <TMDBLogoImg src={TMDMBogo} alt="tmdb-logo" />
     </Content>
