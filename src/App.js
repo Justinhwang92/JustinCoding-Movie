4ae12f7013b49e38e6088f02864ac4a79908d125
Justin <justinhwang92@gmail.com>
2021-08-07 13:48:36 -0400

    Add UserProvider

--- src/App.js
+++ src/App.js
@@ -6,18 +6,22 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Movie from "./components/Movie";
 import NotFound from "./components/NotFound";
+// Context
+import UserProvider from "./context";
 // styles
 import { GlobalStyle } from "./GlobalStyle";
 
 const App = () => (
   <Router>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/:movieId" element={<Movie />} />
-      <Route path="/*" element={<NotFound />} />
-    </Routes>
-    <GlobalStyle />
+    <UserProvider>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:movieId" element={<Movie />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+      <GlobalStyle />
+    </UserProvider>
   </Router>
 );
 
